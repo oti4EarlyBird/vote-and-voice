8bf4781f57021f2cae603859f0f16fee1b1e5ba5
rwanda95do <dlsdnr2379@naver.com>
2025-09-08 11:24:48 +0900

    사용자/관리자 셋팅 중..

--- src/main/java/com/vvs/platform/controller/user/UserController.java
+++ src/main/java/com/vvs/platform/controller/user/UserController.java
@@ -1,5 +1,14 @@
 package com.vvs.platform.controller.user;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@RequestMapping("/")
+	public String index() {
+		//ModelAndView
+		return "user/index";
+	}
 }
