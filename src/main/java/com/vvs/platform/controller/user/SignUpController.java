938b231c23dd2d549831f205d291f42f376cd1f2
Jhp02 <parkjihae698@gmail.com>
2025-09-10 08:26:16 +0900

    0910 회원가입 헤더푸터 적용중

--- src/main/java/com/vvs/platform/controller/user/SignUpController.java
+++ src/main/java/com/vvs/platform/controller/user/SignUpController.java
@@ -1,10 +1,13 @@
 package com.vvs.platform.controller.user;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user")
@@ -12,7 +15,7 @@ public class SignUpController {
 
     // LoginController에서 공유
     private static final HashMap<String, String> users = LoginController.users;
-
+ 
     // 회원가입 폼(GET)
     @GetMapping("/signup")
     public ModelAndView showSignUpForm() {
