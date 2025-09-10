e260b2d5530bbcf1f0f7deaf805d8735af776b58
1suckk <cwsgcd@gmail.com>
2025-09-10 18:16:31 +0900

    feature: 0910 1745 법안 게시판 작성 폼 모달 DB 연결 외 2건

--- src/main/java/com/vvs/platform/controller/admin/BillModalController.java
+++ src/main/java/com/vvs/platform/controller/admin/BillModalController.java
@@ -1,42 +1,34 @@
 package com.vvs.platform.controller.admin;
 
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.vvs.platform.dao.admin.AdminBillsDAO;
 import com.vvs.platform.dto.admin.AdminBillsDTO;
 import com.vvs.platform.dto.admin.PageDTO;
 import com.vvs.platform.service.admin.AdminBillsService;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+//뷰를 불러오지 않는 @Restcontroller 집합 
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
-public class BillController {
+public class BillModalController {
 	
 	private final AdminBillsService adminBillsService;
 	
-	@GetMapping("/bill")
-	public ModelAndView billList() {
-		ModelAndView mav = new ModelAndView();
+	@GetMapping("/billModalSearch")
+	public Map<String, Object> ModalSearch(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "10") int listSize) {
+		PageDTO pageDTO = new PageDTO(pageNum, listSize);
 		
-		mav.setViewName("admin/adminBillList");
-		return mav;
-	}
-	
-	@GetMapping("/billWriteForm")
-	public ModelAndView writeForm() {
-		ModelAndView mav = new ModelAndView();
+		Map<String, Object> result = adminBillsService.getBillInModalSearch(pageDTO);
 		
-		mav.setViewName("admin/adminBillForm");
-		return mav;
+		return result;
 	}
 }
